06ad29df236b4bfe40554f6686192d3525b03d0e
Jose Garcia <jargarcias@unitec.edu>
2024-03-15 00:00:29 -0600

    Update button.jsx

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -5,7 +5,7 @@ import React from "react"
 
 const button = (props) => {
     return (
-        <Button onClick={<Link href={"/blankPage"}} />Login</Button>
+        <Button onClick={<Link href={"/blankPage"}></Link>} >Login</Button>
       );
 };
 
